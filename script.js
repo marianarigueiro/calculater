cf92e6a5a0bec5f752b6a485eb92dfec4c5a50c6
Mariana de O. Rigueiro <166918439+marianarigueiro@users.noreply.github.com>
2024-10-22 18:05:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculate (){
+
+    //entrada
+    const n1 =parseFloat(document.getElementById("n1").value);
+    const n2 =parseFloat(document.getElementById("n2").value);
+    const operation =parseFloat(document.getElementById("operation").value);
+    let result;
+
+    //condição
+    if ( isNaN(n1) || isNaN(n2)) //verificar se o número é válido
+        {
+        result = "por favor, digite um valor válido.";
+        }
+        else{
+
+            swicht(operative);{
+                case "add":
+            }
+        }
+}
